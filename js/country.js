146da3c80bec0eee0cc5e9383dafe73b93689c4e
Artem Zakharchuk <zakharchukartem18@gmail.com>
2026-01-08 16:55:10 +0100

    Aktualny stan projektu

--- js/country.js
+++ js/country.js
@@ -1,21 +1,18 @@
-// Pobieramy parametry z adresu URL
 const params = new URLSearchParams(window.location.search);
-
-// Odczytujemy przekazane dane
 const phrase = params.get("phrase");
 const country = params.get("country");
 
-// Elementy HTML
+
 const phraseText = document.getElementById("phraseText");
 const countryText = document.getElementById("countryText");
 const countryImage = document.getElementById("countryImage");
 
-// Wyświetlamy tekst
+
 phraseText.textContent = "Phrase: " + phrase;
 countryText.innerHTML = `Country: ${country}
 <img src="img/flags/${country}.png" alt="${country} flag" class="inline-flag">`;
 
-// Obrazy przypisane do krajów
+
 const images = {
     poland: ["poland1.jpg", "poland2.jpg", "poland3.jpg"],
     usa: ["usa1.jpg", "usa2.jpg", "usa3.jpg"],
@@ -23,7 +20,7 @@ const images = {
     japan: ["japan1.jpg", "japan2.jpg", "japan3.jpg"]
 };
 
-// Kolory tła dla krajów (HEX)
+
 const backgrounds = {
     poland: "#f1f5f9",
     usa: "#0f172a",
@@ -31,14 +28,11 @@ const backgrounds = {
     japan: "#fde68a",
 };
 
-// Ustawiamy kolor tła strony
 document.body.style.backgroundColor = backgrounds[country];
 
-// Losujemy obraz
 const randomIndex = Math.floor(Math.random() * images[country].length);
 countryImage.src = "img/" + images[country][randomIndex];
 
-// Obrót kompasu zależy od długości tekstu
 const compass = document.querySelector(".compass");
 
 if (compass && phrase) {
--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-// Pobieramy formularz po id
 let form = document.getElementById("countryForm");
-// Nasłuchujemy zdarzenie wysłania formularza
+
 form.addEventListener("submit", function (event) {
-    // Zatrzymujemy domyślne wysyłanie formularza
+   
     event.preventDefault();
-    // Pobieramy tekst wpisany przez użytkownika
+
     let phrase = document.getElementById("phraseInput").value;
     // Pobieramy wybrany kraj
     let selectedCountry = document.querySelector('input[name="country"]:checked');
-    // Sprawdzamy, czy użytkownik wybrał kraj
+
     if (selectedCountry == null) {
         alert("Please choose a country.");
         return;
     }
-
-    // Pobieramy wartość wybranego kraju
+    
     let country = selectedCountry.value;
-    // Przechodzimy do drugiej strony z parametrami w URL
     window.location.href = "country.html?phrase=" + encodeURIComponent(phrase) + "&country=" + country;
 });
